f28b16b1144d0f5428789bf19d4c4ec0808d7795
Eduardo <eduzpa8@gmail.com>
2024-04-26 14:48:38 -0400

    GreenStride1

---
+++ index.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const lastName = document.getElementById("lastName");
+const subject = document.getElementsByClassName("subject");
+const phone = document.getElementById("phone");
+const zip= document.getElementById("zip");
+
+function sendEmail(){
+
+    const bodyMessage = `Full Name: ${fullName.value} ${lastName.value}<br> Phone Number: ${phone.value}<br>
+    Zip Code: ${zip.value}`;
+
+    Email.send({
+        SecureToken:"ee64494f-42ba-44fc-9b8a-ffdb5ad511c0",
+        To : 'greenstridemarketing@gmail.com',
+        From : "greenstridemarketing@gmail.com",
+        Subject : "New client!",
+        Body : bodyMessage
+    }).then(
+        message =>{
+            if(message == "OK" ){
+                Swal.fire({
+                    title: "Thank You",
+                    text: "Message sent succesfully!",
+                    icon: "success"
+                });
+            }
+        }
+    );
+}
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+    sendEmail();
+});
